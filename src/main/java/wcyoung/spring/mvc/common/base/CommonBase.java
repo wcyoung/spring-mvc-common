778e8ccbdc3dd5c3a62ba52efe74b64a45bc7770
Chiyoung Won <wcyoung98@gmail.com>
2020-01-04 16:28:44 +0900

    Update CommonBase

--- src/main/java/wcyoung/spring/mvc/common/base/CommonBase.java
+++ src/main/java/wcyoung/spring/mvc/common/base/CommonBase.java
@@ -1,10 +1,25 @@
 package wcyoung.spring.mvc.common.base;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import wcyoung.spring.mvc.common.property.ApplicationProperties;
 
 public abstract class CommonBase {
 
     protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Resource
+    private ApplicationProperties applicationProperties;
+
+    protected String getProperty(String key) {
+        return applicationProperties.getProperty(key);
+    }
+
+    protected String getProperty(String key, String defaultValue) {
+        return applicationProperties.getProperty(key, defaultValue);
+    }
 
 }
